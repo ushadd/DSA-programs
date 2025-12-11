16478052ea44bde69656063738a6eea5dd7ae7b5
Usha <ushadd05@gmail.com>
2025-12-11 10:53:22 +0530

    third commit

--- src/Array.java
+++ src/Array.java
@@ -2,7 +2,7 @@ public class Array
 {
     public static void main(String[]args)
     {
-        int array[]={ 4,6,8,2,9,1,5};
+        int array[]={ 4,6,8,2,9,1,5,10};
         for(int i=0;i<array.length;i++) //for loop
         {
             System.out.println(array[i]);
